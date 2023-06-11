0c94b24a2e2dbf492b11c40f79422f5ec0e49c73
anveshjarabani <anveshjarabani@gmail.com>
2023-06-10 17:08:36 -0700

    updates

--- linkedin.py
+++ linkedin.py
@@ -1,71 +1,160 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-import json
+import json,time,random
+from selenium.webdriver.common.proxy import Proxy,ProxyType as pt
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+user='test_1124'
+passwrd='xsdf_~#@sdf'
+
+# Fetch a random proxy from ProxyMesh
+proxy_mesh_url = 'http://proxy.proxy-mesh.com:31280'  # Replace with the ProxyMesh URL
+proxy_mesh_proxy_list = ['proxy1.proxy-mesh.com:31280', 'proxy2.proxy-mesh.com:31280']  # Add more proxies if available
+random_proxy = random.choice(proxy_mesh_proxy_list)
+# Configure the proxy server
+proxy = Proxy()
+proxy.proxy_type = pt.MANUAL
+proxy.http_proxy = f'{user}:{passwrd}@{random_proxy}'
+proxy.ssl_proxy = f'{user}:{passwrd}@{random_proxy}'
+# Create a desired capabilities object and set the proxy
+capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+proxy.add_to_capabilities(capabilities)
 chromeOptions=webdriver.ChromeOptions()
-driver=webdriver.Chrome('sldr.exe',options=chromeOptions)
-driver.implicitly_wait(5)
+driver=webdriver.Chrome('sldr.exe',options=chromeOptions,desired_capabilities=capabilities)
+css=By.CSS_SELECTOR
+find=driver.find_element
+finds=driver.find_elements
+located=EC.presence_of_all_elements_located
+clkable=EC.element_to_be_clickable
+# driver.implicitly_wait(3)
 driver.maximize_window()
 driver.get('https://www.linkedin.com/')
-elms=driver.find_elements(By.TAG_NAME,'button')
+elms=finds(By.TAG_NAME,'button')
 if len(elms)==0:
-    driver.refresh()
-driver.find_element(By.CSS_SELECTOR,"[id='session_key']").send_keys("anveshjarabani@gmail.com")
-driver.find_element(By.CSS_SELECTOR,"[id='session_password']").send_keys(json.load(open('encrypt.json','r'))['password'])
-driver.find_element(By.CSS_SELECTOR,"[data-id*='sign-in-form__submit-btn']").click()
-WebDriverWait(driver,25).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"[title*='Jobs']")))
-driver.find_element(By.CSS_SELECTOR,"[title*='Jobs']").click()
-WebDriverWait(driver,25).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"[id*='jobs-search-box-keyword']")))
-driver.find_element(By.CSS_SELECTOR,"[id*='jobs-search-box-keyword']").send_keys('data engineer\n')
-WebDriverWait(driver,25).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"[aria-label*='Easy Apply filter.']")))
-driver.find_element(By.CSS_SELECTOR,"[aria-label*='Easy Apply filter.']").click()
-# WebDriverWait(driver,25).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"[class*='jobs-apply-button']")))
+    driver.get('https://www.linkedin.com/')
+time.sleep(2)
+find(css,"input[id='session_key']").send_keys("anveshjarabani@gmail.com")
+path=r'C:\Users\anves\Downloads\job automation\{}'
+find(css,"input[id='session_password']").send_keys(json.load(open(path.format('encrypt.json'),'r'))['password'])
+find(css,"button[data-id*='sign-in-form__submit-btn']").click()
+wait(driver,100).until(located((css,"[title*='Jobs']"))) 
+find(css,"[title*='Jobs']").click()
+wait(driver,25).until(located((css,"[id*='jobs-search-box-keyword']")))
+find(css,"[id*='jobs-search-box-keyword']").send_keys('data engineer\n')
+wait(driver,25).until(located((css,"[aria-label*='Easy Apply filter.']")))
+find(css,"[aria-label*='Easy Apply filter.']").click()
+# wait(driver,25).until(located((css,"[class*='jobs-apply-button']")))
+def select_yes(elem):
+    try:
+        elem.find_element(css,
+        "label[data-test-text-selectable-option__label='Yes']").click()
+    except:
+        try:
+            elem.find_element(
+            css,"select[id*='text-entity']").click()
+            elem.find_element(css,"option[value*='Yes']").click()
+        except:
+            elem.find_element(css,"input[type*='text']").send_keys('Yes')
 def apply_job(i):
-    apply_buttons = driver.find_elements(By.CSS_SELECTOR, "button[aria-label*='Easy Apply to']")
+    time.sleep(2)
+    apply_buttons = finds(css, "div[class*='jobs-apply-button--']")
     if len(apply_buttons)!=0:
         apply_buttons[0].click()
     else:
         return
-    flg = True
-    while flg:
-        try:
-            driver.find_element(
-                By.CSS_SELECTOR, "[aria-label*='Continue to next step']").click()
-            invalid_elms = driver.find_elements(
-                By.CSS_SELECTOR, 'div[aria-invalid="true"]')
+    while True:
+        if finds(css, 'div[aria-invalid="true"]'):
+            invalid_elms = finds(
+            css, 'div[aria-invalid="true"]')
             for elem in invalid_elms:
                 try:
-                    elem.find_element(
-                        By.CSS_SELECTOR, "input[type*='text']").send_keys(5)
+                    if 'City' in elem.text:
+                        elem.find_element(
+                        css, "input[type*='text']").send_keys('Chandler, Arizona, United States')
+                        time.sleep(.5)
+                        elem.find_element(css, "input[type*='text']").send_keys(Keys.TAB)
+                    elif 'years' in elem.text:
+                        elem.find_element(
+                        css, "input[type*='text']").send_keys(5)
+                    elif 'salary' in elem.text or 'pay' in elem.text:
+                        elem.find_element(
+                        css, "input[type*='text']").send_keys(190000)
+                    elif 'Name*' in elem.text:
+                        elem.find_element(
+                        css, "input[type*='text']").send_keys('Anvesh Jarabani')
+                    elif 'commut' in elem.text:
+                        select_yes(elem)
+                    elif 'Do you have' in elem.text and 'experience' in elem.text:
+                        select_yes(elem)
+                    elif 'eligible to work' in elem.text:
+                        select_yes(elem)
+                    elif 'sponsorship' in elem.text or 'Sponsorship' in elem.text:
+                        select_yes(elem)
+                    elif 'How did you hear' in elem.text:
+                        elem.find_element(
+                        css, "input[type*='text']").send_keys('Linkedin')  
+                    elif 'you able to begin' in elem.text or 'When can you start' in elem.text:
+                        elem.find_element(
+                        css, "input[type*='text']").send_keys('2 WEEKS FROM OFFER')  
+                    elif 'I Agree Terms' in elem.text:
+                        elem.find_element(css,'label').click()
                 except:
-                    pass
-                # try:
-
-
+                    pass      
+        elif 'Self-Identification' in find(css,"div[class*='jobs-easy-apply-content']").text:
+            options=find(css,"div[class*='jobs-easy-apply-content']").find_elements(css,"select")
+            for x in options:
+                if 'veteran' in x.text:
+                    x.find_element(css,"option[value*='Select an option']").click()
+                    x.find_element(css,"option[value*='I am not']").click()
+                elif 'Race' in x.text:
+                    x.find_element(css,"option[value*='Select an option']").click()
+                    x.find_element(css,"option[value*='Asian']").click()
+                elif 'Male' in x.text:
+                    x.find_element(css,"option[value*='Select an option']").click()
+                    x.find_element(css,"option[value*='Male']").click()
+                elif 'Disability' in x.text:
+                    x.find_element(css,"option[value*='Select an option']").click()
+                    x.find_element(css,"option[value*='No']").click()
+            if finds(css, "button[aria-label*='Continue to next step']"):
+                find(css, "[aria-label*='Continue to next step']").click()
+        elif finds(css, "button[aria-label*='Continue to next step']"):
+            find(css, "[aria-label*='Continue to next step']").click()
+        elif finds(css,"button[aria-label*='Review your application']"):
+            find(css,"button[aria-label*='Review your application']").click()
+        elif finds(css,"button[aria-label*='Submit application']"): 
+            if finds(css,"label[for*='follow-company']"):
+                find(css,"label[for*='follow-company']").click()
+            find(css,"button[aria-label*='Submit application']").click()
+            break
+    while True:
+        try:
+            find(css, "button[aria-label*='Dismiss']").click() 
+            break
         except:
-            flg = False
-    try:
-        driver.find_element(By.CSS_SELECTOR,
-                        "[aria-label*='Review your application']").click()
-    except:
-        pass
-    try:
-        driver.find_element(By.CSS_SELECTOR,
-                    "[aria-label*='Submit application']").click()
-    except:
-        pass
-    driver.find_element(By.CSS_SELECTOR, "button[aria-label*='Dismiss']").click()
-
-jobs = driver.find_elements(
-    By.CSS_SELECTOR, "div[class*='job-card-container--clickable']")
-
-for r,i in enumerate(jobs):
-    apply_job(i)
-    jobs = driver.find_elements(
-        By.CSS_SELECTOR, "div[class*='job-card-container--clickable']")
-    element = jobs[r+1]
-    element.click()
+            pass
+page=1
+while page<40:
+    jobs=finds(css,"li[class*='jobs-search-results__list-item']")
+    driver.execute_script("arguments[0].scrollIntoView()",jobs[10])
+    driver.execute_script("arguments[0].scrollIntoView()",jobs[-1])
+    time.sleep(2)
+    jobs=finds(css,"li[class*='jobs-search-results__list-item']")
+    jobs=[i for i in jobs if 'Easy Apply' in i.text]
+    for r,i in enumerate(jobs):
+            element = jobs[r]
+            while True:
+                try:
+                    element.click()
+                    break
+                except:
+                    jobs=finds(css,"li[class*='jobs-search-results__list-item']")
+                    jobs=[i for i in jobs if 'Easy Apply' in i.text]
+                    element=jobs[r]
+            apply_job(i)    
+    page+=1
+    time.sleep(2)
+    find(css,"li[data-test-pagination-page-btn='{}']".format(page)).click()
 
 
 
