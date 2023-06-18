94493b394296d7f6b0f10748eaee07a709202bb0
anveshjarabani <anveshjarabani@gmail.com>
2023-06-18 00:26:29 -0700

    updates

--- json_sandbox.py
+++ json_sandbox.py
@@ -1,7 +1,9 @@
 import json,os
+from rich import print
 cur_dir=os.path.realpath(os.path.join(os.getcwd(),os.path.dirname(__file__)))
 with open(os.path.join(cur_dir,'job_data.json'),'r') as f:
     data = json.load(f)
+data['Form Data']=[i for i in data['Form Data'] if i is not None]
 for i in data['Form Data']:
     for j in i:
         y=j.split('\n')
@@ -12,7 +14,7 @@ for i in data['Form Data']:
             if 'visa' in string:
                 indx=y.index(string)
                 print(y[indx])
-            if 'City' in string:
+            if 'city' in string.lower():
                 indx=y.index(string)
                 print(y[indx])
 for i in data['Desc']:
